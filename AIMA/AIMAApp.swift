a388954245df4acfcf6e8c8a0f91353883d91189
Alice Huang <viento.a13@gmail.com>
2023-10-19 11:56:50 +0800

    Initial Commit

---
+++ AIMA/AIMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMAApp.swift
+//  AIMA
+//
+//  Created by 黃允謙 on 2023/10/19.
+//
+
+import SwiftUI
+
+@main
+struct AIMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIMA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AIMA
+//
+//  Created by 黃允謙 on 2023/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
