fddc7a9f00cc607f5f123ec6abc8baf781c4b915
Alice Huang <viento.a13@gmail.com>
2023-11-27 20:02:06 +0800

    Rendered Membership, ChangePwd and ContactUs.

---
+++ AIMA/View/Member/ChangePwd.swift
@@ -0,0 +1,8 @@
+//
+//  ChangePwd.swift
+//  AIMA
+//
+//  Created by 黃允謙 on 2023/11/27.
+//
+
+import Foundation
---
+++ AIMA/View/Member/ContactUs.swift
@@ -0,0 +1,8 @@
+//
+//  ContactUs.swift
+//  AIMA
+//
+//  Created by 黃允謙 on 2023/11/27.
+//
+
+import Foundation
